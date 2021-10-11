ae7023b2f3bf4b1edc83d2c7db6428612f3ef025
Aleksandr B <aleksandr.b@smplt.net>
2021-10-11 19:05:33 +0400

    1) Added change detector for finding difference between two BTreeMaps 2) Removed warnings + tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ mod my_no_sql_entity;
 mod my_no_sql_tcp_connection;
 mod subscribers;
 mod tcp;
+mod utils;
 
 pub use my_no_sql_entity::MyNoSqlEntity;
 pub use my_no_sql_tcp_connection::MyNoSqlTcpConnection;
+pub use utils::ChangeDetector;
---
+++ src/utils/mod.rs
@@ -0,0 +1,3 @@
+pub mod change_detector;
+
+pub use change_detector::ChangeDetector;
