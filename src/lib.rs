13dfa376e7a63f47c6f3ce6e6a611fe6e85a7e6f
amigin <amigin@gmail.com>
2022-09-04 16:28:47 +0300

    Synced after merge

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,4 @@ mod subscribers;
 mod tcp_events;
 
 pub use my_no_sql_tcp_connection::MyNoSqlTcpConnection;
-<<<<<<< HEAD
 pub use subscribers::{MyNoSqlDataRaderCallBacks, MyNoSqlDataReader, MyNoSqlDataReaderData};
-=======
-pub use subscribers::MyNoSqlDataReader;
->>>>>>> 47130bea0ec43d332d7ba5cc6c29572c8cdaab6c
